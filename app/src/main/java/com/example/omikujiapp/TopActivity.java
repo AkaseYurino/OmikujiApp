78d53a212ebf1bb786533cfd9ea8aeef2540e174
WINDOWS\y0324 <yurino.akase@itpm-gk.com>
2024-10-19 16:26:21 +0900

    コード記述

--- app/src/main/java/com/example/omikujiapp/TopActivity.java
+++ app/src/main/java/com/example/omikujiapp/TopActivity.java
@@ -1,6 +1,8 @@
 package com.example.omikujiapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,18 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        startBtn = findViewById(R.id.start_button);
+        startBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                //スタートボタンタップ処理
+                //TODO：おみくじの抽選を実行
+                //TODO：結果画面に遷移
+
+            }
         });
     }
 }
