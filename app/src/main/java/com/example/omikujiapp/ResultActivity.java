d771f80c5378ede5ab5dd2bcfe47b8cf50e594fe
WINDOWS\y0324 <yurino.akase@itpm-gk.com>
2024-10-19 16:26:35 +0900

    コード記述

--- app/src/main/java/com/example/omikujiapp/ResultActivity.java
+++ app/src/main/java/com/example/omikujiapp/ResultActivity.java
@@ -1,6 +1,9 @@
 package com.example.omikujiapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class ResultActivity extends AppCompatActivity {
 
+    public static String KEY_RESULT_NUMBER = "com.example.omikujiap.key_result_number";
+
+    public static String[] OMIKUJI_PATTERN = {"大吉", "中吉", "小吉", "吉", "末吉", "凶", "大凶"};
+    public static int[] OMIKUJI_IMG_RES_ID = {
+            R.drawable.great_blessing, R.drawable.middle_blessing, R.drawable.small_blessing,
+            R.drawable.blessing, R.drawable.blessing, R.drawable.uncertain_luck,
+            R.drawable.curse, R.drawable.great_curse
+    };
+    public static int OMIKUJI_MAX_NUMBER= OMIKUJI_PATTERN.length; //7
+
+    public static Intent newIntent(Context context, int resultNum) {
+        Intent intent = new Intent(context, ResultActivity.class);
+        intent.putExtra(KEY_RESULT_NUMBER, resultNum);
+        return intent;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +40,21 @@ public class ResultActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        int resultNum =
+                getIntent().getIntExtra(KEY_RESULT_NUMBER, -1);
+
+        if(resultNum == -1){
+            throw new RuntimeException("result number is not found.");
+        }
+
+        //TODO：データを受け取る（結果の番号）
+        //TODO：結果に応じて表示を変える
+
+        String resultTitle = OMIKUJI_PATTERN[resultNum];
+        int resultImResId= OMIKUJI_IMG_RES_ID[resultNum];
+
+       Log.d("ResultActivity", "resultTitle" + resultTitle);
+
     }
 }
--- app/src/main/java/com/example/omikujiapp/TopActivity.java
+++ app/src/main/java/com/example/omikujiapp/TopActivity.java
@@ -1,5 +1,6 @@
 package com.example.omikujiapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -10,7 +11,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class TopActivity extends AppCompatActivity {
 
     private FrameLayout startBtn;
 
@@ -18,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_top);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -32,7 +35,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //スタートボタンタップ処理
                 //TODO：おみくじの抽選を実行
-                //TODO：結果画面に遷移
+                Random randomGenerator = new Random();
+                int resultNum = randomGenerator.nextInt(ResultActivity.OMIKUJI_MAX_NUMBER);
+
+                //結果画面に遷移
+                startActivity(ResultActivity.newIntent(TopActivity.this, resultNum));
 
             }
         });
